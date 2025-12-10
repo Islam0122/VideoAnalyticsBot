2ae6e3916299a7461e4b42ea7ca13aaeca9014d0
islam0122 <duishobaevislam01@gmail.com>
2025-12-10 22:01:42 +0600

    config.py --> fix

--- src/config.py
+++ src/config.py
@@ -18,14 +18,16 @@ class Config:
     def DATABASE_URL(self):
         return (
             f"postgresql://{self.DB_USER}:{self.DB_PASSWORD}"
-            f"@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}?ssl=false"
+            f"@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+            f"?sslmode=require"
         )
 
     @property
     def ASYNC_DATABASE_URL(self):
         return (
             f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASSWORD}"
-            f"@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}?ssl=false"
+            f"@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+            f"?sslmode=require"
         )
 
     LLM_PROVIDER = os.getenv("LLM_PROVIDER", "openai")
